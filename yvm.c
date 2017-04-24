0ff6a7f7836620fd1777e0f1a997b10596efe99e
zy <zeng.yu.1993@gmail.com>
2017-04-24 15:40:11 +0800

    Move defination of operaton code to yvm.h

--- yvm.c
+++ yvm.c
@@ -1,56 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "yvm.h"
 
 // user define type
 typedef unsigned int uint;
-
-#define halt     0x00
-#define nop      0x10
-
-// condition move
-#define cmv_mask 0x20
-#define rrmovl   (cmv_mask | 0x00)
-#define cmovle   (cmv_mask | 0x01)
-#define cmovl    (cmv_mask | 0x02)
-#define cmove    (cmv_mask | 0x03)
-#define cmovne   (cmv_mask | 0x04)
-#define cmovge   (cmv_mask | 0x05)
-#define cmovg    (cmv_mask | 0x06)
-
-#define irmovl   0x30
-#define rmmovl   0x40
-#define mrmovl   0x50
-
-// numric and logic operation
-#define opl_mask 0x60
-#define addl     (opl_mask | 0x00)
-#define subl     (opl_mask | 0x01)
-#define andl     (opl_mask | 0x02)
-#define xorl     (opl_mask | 0x03)
-
-// condition jump
-#define jmp_mask 0x70
-#define jmp      (jmp_mask | 0x00)
-#define jle      (jmp_mask | 0x01)
-#define jl       (jmp_mask | 0x02)
-#define je       (jmp_mask | 0x03)
-#define jne      (jmp_mask | 0x04)
-#define jge      (jmp_mask | 0x05)
-#define jg       (jmp_mask | 0x06)
-
-#define call     0x80
-#define ret      0x90
-
-#define pushl    0xa0
-#define popl     0xb0
-
-
-// stat code
-#define AOK 1
-#define HLT 2  
-#define ADR 3  // invalid memory address
-#define INS 4  // invalid operation
-
 
 // register file
 static uint eax;
