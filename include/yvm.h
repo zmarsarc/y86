39aa34427ff00849e39902b89d62751da4ecfea0
zy <zeng.yu.1993@gmail.com>
2017-04-28 15:03:56 +0800

    Split instructs into small methods

--- include/yvm.h
+++ include/yvm.h
@@ -43,6 +43,20 @@
 #define pushl    0xa0
 #define popl     0xb0
 
+// register if
+#define EAX (unsigned)0x0
+#define ECX (unsigned)0x1
+#define EDX (unsigned)0x2
+#define EBX (unsigned)0x3
+#define ESP (unsigned)0x4
+#define EBP (unsigned)0x5
+#define ESI (unsigned)0x6
+#define EDI (unsigned)0x7
+#define PC  (unsigned)0x8
+#define FLAG (unsigned)0x9
+#define STAT (unsigned)0xA
+#define NREG (unsigned)0xF
+
 // flag mask
 #define F_OVER (unsigned int)0x00000001
 #define F_SIGN (unsigned int)0x00000002
@@ -57,9 +71,12 @@
 typedef unsigned int RESULT;
 #define S_OK              (RESULT)0x0000
 #define S_HALT            (RESULT)(S_OK | 0x0001)
+#define S_JMP             (RESULT)(S_OK | 0x0002)
+#define S_FALSE_COND      (RESULT)(S_OK | 0x0003)
 #define E_ERROR           (RESULT) 0x1000
 #define E_INVALID_REG_ID  (RESULT)(E_ERROR | 0x0001)
 #define E_INVALID_OPT     (RESULT)(E_ERROR | 0x0002)
+#define E_INVALID_ADDRESS (RESULT)(E_ERROR | 0x0003)
 
 
 // context file
@@ -72,9 +89,16 @@ typedef struct __vm_contetxt {
     unsigned int ebp;  // 5
     unsigned int esi;  // 6
     unsigned int edi;  // 7
-    unsigned int pc;
-    unsigned int flag;
-    unsigned int stat;
+    unsigned int pc;   // A
+    unsigned int flag; // B
+    unsigned int stat; // C
+	
+	// reserved register address
+	unsigned int __r1; // D
+	unsigned int __r2; // E
+	unsigned int __r3; // F
+
+	// memory pointer
     unsigned char* memory;
     unsigned int m_size;
 } vm_context;
--- src/yvm.cpp
+++ src/yvm.cpp
@@ -1,54 +1,59 @@
 #include <assert.h>
 #include "yvm.h"
 
-#define SET_OF (cur_context->flag |= 0x00000001)
-#define CLR_OF (cur_context->flag &= 0xFFFFFFFE)
-#define TST_OF (uint)(cur_context->flag & 0x00000001)
+#define SET_OF (context->flag |= F_OVER)
+#define CLR_OF (context->flag &= ~F_OVER)
+#define TST_OF (uint)(context->flag & F_OVER)
 
-#define SET_SF (cur_context->flag |= 0x00000002)
-#define CLR_SF (cur_context->flag &= 0xFFFFFFFD)
-#define TST_SF (uint)((cur_context->flag & 0x00000002) >> 1)
+#define SET_SF (context->flag |= F_SIGN)
+#define CLR_SF (context->flag &= ~F_SIGN)
+#define TST_SF (uint)(context->flag & F_SIGN)
 
-#define SET_ZF (cur_context->flag |= 0x00000004)
-#define CLR_ZF (cur_context->flag &= 0xFFFFFFFB)
-#define TST_ZF (uint)((cur_context->flag & 0x00000004) >> 2)
+#define SET_ZF (context->flag |= F_ZERO)
+#define CLR_ZF (context->flag &= ~F_ZERO)
+#define TST_ZF (uint)(context->flag & F_ZERO)
+
+#define REGISTER_FILE ((unsigned int*)(context))
+#define MEMORY_BASE ((unsigned char*)context->memory)
+#define MEMORY32(X) (*((unsigned int*)(MEMORY_BASE + (X))))
 
 // user define type
 typedef unsigned int uint;
 typedef unsigned char ubyte;
+typedef RESULT(*pINSTRUCT)(vm_context, const uint, const uint, const uint);
 
 static uint nog;  // F register never used
 
 // const
 static const uint uint_max = (uint)0xFFFFFFFF;
 
-static RESULT select_reg(vm_context* cur_context, const ubyte id, uint** reg) {
-    assert(cur_context);
+static RESULT select_reg(vm_context* context, const ubyte id, uint** reg) {
+    assert(context);
 
     switch (id) {
         case 0x0:
-            *reg = &(cur_context->eax);
+            *reg = &(context->eax);
             break;
         case 0x1:
-            *reg = &(cur_context->ecx);
+            *reg = &(context->ecx);
             break;
         case 0x2:
-            *reg = &(cur_context->edx);
+            *reg = &(context->edx);
             break;
         case 0x3:
-            *reg = &(cur_context->ebx);
+            *reg = &(context->ebx);
             break;
         case 0x4:
-            *reg = &(cur_context->esp);
+            *reg = &(context->esp);
             break;
         case 0x5:
-            *reg = &(cur_context->ebp);
+            *reg = &(context->ebp);
             break;
         case 0x6:
-            *reg = &(cur_context->esi);
+            *reg = &(context->esi);
             break;
         case 0x7:
-            *reg = &(cur_context->edi);
+            *reg = &(context->edi);
             break;
         case 0xF:
             *reg = &nog;
@@ -59,68 +64,68 @@ static RESULT select_reg(vm_context* cur_context, const ubyte id, uint** reg) {
     return S_OK;
 }
 
-static RESULT split_regs(vm_context* cur_context, const ubyte reg_file, uint** reg_a, uint** reg_b) {
-    if (select_reg(cur_context, (ubyte)((reg_file & 0xF0) >> 4), reg_a)) return E_INVALID_REG_ID;
-    if (select_reg(cur_context, (ubyte)(reg_file & 0x0F), reg_b)) return E_INVALID_REG_ID;
+static RESULT split_regs(vm_context* context, const ubyte reg_file, uint** reg_a, uint** reg_b) {
+    if (select_reg(context, (ubyte)((reg_file & 0xF0) >> 4), reg_a)) return E_INVALID_REG_ID;
+    if (select_reg(context, (ubyte)(reg_file & 0x0F), reg_b)) return E_INVALID_REG_ID;
     return S_OK;
 }
 
-extern "C" RESULT process(vm_context* cur_context, const ubyte opt, const ubyte regs, const uint arg) {
-    assert(cur_context);
-    if (cur_context->stat == HLT) return S_HALT;
+extern "C" RESULT process(vm_context* context, const ubyte opt, const ubyte regs, const uint arg) {
+    assert(context);
+    if (context->stat == HLT) return S_HALT;
     uint *reg_a = 0;
     uint *reg_b = 0;
     uint tmp = 0;
 
     switch (opt) {
         case cmovg:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = (!TST_SF) ? *reg_a : *reg_b;
             break;
         case cmovge:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = (!TST_SF || TST_ZF) ? *reg_a : *reg_b;
             break;
         case cmovne:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = (!TST_ZF) ? *reg_a : *reg_b;
             break;
         case cmove:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = (TST_ZF) ? *reg_a : *reg_b;
             break;
         case cmovl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = (TST_SF) ? *reg_a : *reg_b;
             break;
         case cmovle:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             // b <- a
             *reg_b = (TST_SF || TST_ZF) ? *reg_a : *reg_b;
             break;
         case jg:
-            if (!TST_SF) cur_context->pc = arg;
+            if (!TST_SF) context->pc = arg;
             break;
         case jge:
-            if (!TST_SF || TST_ZF) cur_context->pc = arg;
+            if (!TST_SF || TST_ZF) context->pc = arg;
             break;
         case jne:
-            if (!TST_ZF) cur_context->pc = arg;
+            if (!TST_ZF) context->pc = arg;
             break;
         case je:
-            if (TST_ZF) cur_context->pc = arg;
+            if (TST_ZF) context->pc = arg;
             break;
         case jl:
-            if (TST_SF) cur_context->pc = arg;
+            if (TST_SF) context->pc = arg;
             break;
         case jle:
-            if (TST_SF || TST_ZF) cur_context->pc = arg;
+            if (TST_SF || TST_ZF) context->pc = arg;
             break;
         case jmp:
-            cur_context->pc = arg;
+            context->pc = arg;
             break;
         case xorl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             tmp = *reg_a ^ *reg_b;
             if ((int)tmp < 0) SET_SF; else CLR_SF;
             if (tmp == (uint)0) SET_ZF; else CLR_ZF;
@@ -128,7 +133,7 @@ extern "C" RESULT process(vm_context* cur_context, const ubyte opt, const ubyte 
             *reg_a = tmp;
             break;
         case andl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             tmp = *reg_a | *reg_b;
             if ((int)tmp < 0) SET_SF; else CLR_SF;
             if (tmp == (uint)0) SET_ZF; else CLR_ZF;
@@ -136,7 +141,7 @@ extern "C" RESULT process(vm_context* cur_context, const ubyte opt, const ubyte 
             *reg_a = tmp;
             break;
         case subl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             tmp = (uint)(*reg_a - *reg_b);
             if ((int)*reg_b > (int)*reg_a) SET_OF; else CLR_OF;
             if ((int)tmp < 0) SET_SF; else CLR_SF;
@@ -144,7 +149,7 @@ extern "C" RESULT process(vm_context* cur_context, const ubyte opt, const ubyte 
             *reg_a = tmp;
             break;
         case addl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             tmp = (uint)(*reg_a + *reg_b);
             if ((*reg_a > uint_max - *reg_b) || (*reg_b > uint_max - *reg_a)) SET_OF; else CLR_OF;
             if ((int)tmp < 0) SET_SF; else CLR_SF;
@@ -152,51 +157,253 @@ extern "C" RESULT process(vm_context* cur_context, const ubyte opt, const ubyte 
             *reg_a = tmp;
             break;
         case call:
-            cur_context->pc = arg;
+            context->pc = arg;
             break;
         case ret:
-            cur_context->pc = *(cur_context->memory + cur_context->esp);
-            cur_context->esp = (uint)(cur_context->esp + sizeof(uint));
+            context->pc = *(context->memory + context->esp);
+            context->esp = (uint)(context->esp + sizeof(uint));
             break;
         case mrmovl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
-            tmp = (uint)((arg + *reg_b) % cur_context->m_size);
-            *reg_a = *(uint*)(cur_context->memory + tmp);
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            tmp = (uint)((arg + *reg_b) % context->m_size);
+            *reg_a = *(uint*)(context->memory + tmp);
             break;
         case rmmovl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
-            tmp = (uint)((arg + *reg_b) % cur_context->m_size);
-            *(uint*)(cur_context->memory + tmp) = *reg_a;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            tmp = (uint)((arg + *reg_b) % context->m_size);
+            *(uint*)(context->memory + tmp) = *reg_a;
             break;
         case irmovl:
             tmp = regs | 0xF0;
-            if (split_regs(cur_context, tmp, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, tmp, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             *reg_b = arg;
             break;
         case rrmovl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
             // b <- a
             *reg_b = *reg_a;
             break;
         case popl:
-            if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
-            *reg_a = *((uint*)(cur_context->memory + cur_context->esp));
-            cur_context->esp = (uint)((cur_context->esp + (uint)sizeof(uint)) % cur_context->m_size);
+            if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            *reg_a = *((uint*)(context->memory + context->esp));
+            context->esp = (uint)((context->esp + (uint)sizeof(uint)) % context->m_size);
             break;
         case pushl:
-			if (split_regs(cur_context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
-            cur_context->esp = (uint)((cur_context->esp - (uint)sizeof(uint)) % cur_context->m_size);
-            *(uint*)(cur_context->memory + cur_context->esp) = *reg_a;
+			if (split_regs(context, regs, &reg_a, &reg_b)) return E_INVALID_REG_ID;
+            context->esp = (uint)((context->esp - (uint)sizeof(uint)) % context->m_size);
+            *(uint*)(context->memory + context->esp) = *reg_a;
             break;
         case nop:
             break;
         case halt:
-            cur_context->stat = HLT;
+            context->stat = HLT;
             return S_HALT;
         default:
-            cur_context->stat = INS;
+            context->stat = INS;
             return E_INVALID_OPT;
     }
-    cur_context->stat = AOK;
+    context->stat = AOK;
     return S_OK;
+}
+
+
+
+
+
+
+static RESULT I_PUSHL(vm_context*, const uint, const uint, const uint);
+static RESULT I_POPL(vm_context*, const uint, const uint, const uint);
+
+// INS CODE 0x00
+static RESULT I_HALT(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	context->stat = halt;
+	return S_OK;
+}
+
+// INS CODE 0x10
+static RESULT I_NOP(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	return S_OK;
+}
+
+// INS CODE 0x20
+inline RESULT I_RRMOVL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// move the value stored in register b to register a
+	REGISTER_FILE[reg_a] = REGISTER_FILE[reg_b];
+	return S_OK;
+}
+
+// INS CODE 0x21
+static RESULT I_CMOVLE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_SF || TST_ZF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x22
+static RESULT I_CMOVL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_SF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x23
+static RESULT I_CMOVE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_ZF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x24
+static RESULT I_CMOVNE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_ZF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x25
+static RESULT I_CMOVGE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_SF || TST_ZF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x26
+static RESULT I_CMOVG(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_SF) return I_RRMOVL(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x30
+static RESULT I_IRMOVL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register b <- arg
+	REGISTER_FILE[reg_b] = arg;
+	return S_OK;
+}
+
+// INS CODE 0x40
+static RESULT I_RMMOVL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// arg[register b] <- register a
+	uint offset = (arg + REGISTER_FILE[reg_b]) % context->m_size;
+	MEMORY32(offset) = REGISTER_FILE[reg_a];
+	return S_OK;
+}
+
+// INS CODE 0x50
+static RESULT I_MRMOVL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register a <- arg[register b]
+	uint offset = (arg + REGISTER_FILE[reg_b]) % context->m_size;
+	REGISTER_FILE[reg_a] = MEMORY32(offset);
+	return S_OK;
+}
+
+// INS CODE 0x60
+static RESULT I_ADDL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register a <- register a + register b
+	unsigned result = REGISTER_FILE[reg_a] + REGISTER_FILE[reg_b];
+	if (result < REGISTER_FILE[reg_a] || result < REGISTER_FILE[reg_b]) SET_OF; else CLR_OF;
+	if (result & (unsigned)0x80000000) SET_SF; else CLR_SF;
+	if (result == (unsigned)0x0) SET_ZF; else CLR_ZF;
+	REGISTER_FILE[reg_a] = result;
+	return S_OK;
+}
+
+// INS CODE 0x61
+static RESULT I_SUBL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register a <- register a - register b
+	// equval to register a + (- register b)
+	REGISTER_FILE[reg_b] = ~REGISTER_FILE[reg_b] + (unsigned)0x1;
+	RESULT result = I_ADDL(context, reg_a, reg_b, arg);
+	REGISTER_FILE[reg_b] = ~REGISTER_FILE[reg_b] + (unsigned)0x1;
+	return S_OK;
+}
+
+// INS CODE 0x62
+static RESULT I_ANDL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register a <- register a & register b
+	unsigned result = REGISTER_FILE[reg_a] & REGISTER_FILE[reg_b];
+	if (result & (unsigned)0x80000000) SET_SF; else CLR_SF;
+	if (result == (unsigned)0x0) SET_ZF; else CLR_ZF;
+	REGISTER_FILE[reg_a] = result;
+	return S_OK;
+}
+
+// INS CODE 0x63
+static RESULT I_XORL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	// register a <- register a ^ register b
+	unsigned result = REGISTER_FILE[reg_a] ^ REGISTER_FILE[reg_b];
+	if (result & (unsigned)0x80000000) SET_SF; else CLR_SF;
+	if (result == (unsigned)0x0) SET_ZF; else CLR_ZF;
+	REGISTER_FILE[reg_a] = result;
+	return S_OK;
+}
+
+// INS CODE 0x70
+static RESULT I_JMP(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (arg >= context->m_size) return E_INVALID_ADDRESS;
+	REGISTER_FILE[PC] = arg;
+	return S_JMP;
+}
+
+// INS CODE 0x71
+static RESULT I_JLE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_SF || TST_ZF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x72
+static RESULT I_JL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_SF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x73
+static RESULT I_JE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (TST_ZF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x74
+static RESULT I_JNE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_ZF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x75
+static RESULT I_JGE(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_SF || TST_ZF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x76
+static RESULT I_JG(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	if (!TST_SF) return I_JMP(context, reg_a, reg_b, arg);
+	return S_FALSE_COND;
+}
+
+// INS CODE 0x80
+static RESULT I_CALL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	RESULT result = I_PUSHL(context, PC, NREG, 0);
+	if (S_OK != result) return result;
+	result = I_JMP(context, NREG, NREG, arg);
+	if (S_JMP != result) {
+		I_POPL(context, NREG, NREG, 0);
+		return result;
+	}
+	return S_JMP;
+}
+
+// INS CODE 0x90
+static RESULT I_RET(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	return I_POPL(context, PC, NREG, 0);
+}
+
+// INS CODE 0xA0
+static RESULT I_PUSHL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	unsigned sp_address = (context->esp - 4) % context->m_size;
+	MEMORY32(sp_address) = REGISTER_FILE[reg_a];
+	context->esp = sp_address;
+	return S_OK;
+}
+
+// INS CODE 0xB0
+static RESULT I_POPL(vm_context* context, const uint reg_a, const uint reg_b, const uint arg) {
+	unsigned sp_address = context->esp % context->m_size;
+	REGISTER_FILE[reg_a] = MEMORY32(sp_address);
+	context->esp = sp_address;
+	return (PC == reg_a) ? S_JMP : S_OK;
 }
