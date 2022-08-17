f184e09d494f583ee39f206910f873881f2ad3e4
Zeng Yu <zeng.yu.1993@gmail.com>
2022-08-17 16:37:28 +0800

    [update] 补充注释并调整代码格式

--- Y86/Assemable.cs
+++ Y86/Assemable.cs
@@ -17,17 +17,21 @@
 
 using Y86.Machine;
 
-namespace Y86.Assemable {
+namespace Y86.Assemable
+{
 
-    namespace Errors {
+    namespace Errors
+    {
 
         [Serializable]
-        public class MismatchException: Exception {
+        public class MismatchException : Exception
+        {
 
-            public Token Token {get;}
-            public Token.Types Expect {get;}
+            public Token Token { get; }
+            public Token.Types Expect { get; }
 
-            public MismatchException(Token.Types expect, Token tk): base() {
+            public MismatchException(Token.Types expect, Token tk) : base()
+            {
                 Token = tk;
                 Expect = expect;
             }
@@ -39,19 +43,23 @@ namespace Y86.Assemable {
         }
 
         [Serializable]
-        public class LabelRedefinedException: Exception {
-            public string Label {get;}
-            public LabelRedefinedException(string label): base() => Label = label;
-            public override string ToString() {
+        public class LabelRedefinedException : Exception
+        {
+            public string Label { get; }
+            public LabelRedefinedException(string label) : base() => Label = label;
+            public override string ToString()
+            {
                 return $"label \"{Label}\" is redefined";
             }
         }
     }
 
     // Token 描述基本的符号信息
-    public class Token {
+    public class Token
+    {
 
-        public enum Types {
+        public enum Types
+        {
             EOS,        // end of stream 代表符号流结束
             Whitespace, // 所有空白字元，用于分割两个token以避免二义性
             ID,         // 标识符如main/addl/eax等
@@ -63,10 +71,11 @@ namespace Y86.Assemable {
             Colon,      // 符号 ":"
             Hashtag     // 符号 “#”
         }
-        
-        public Types Type {get;}
 
-        protected Token(Types type) {
+        public Types Type { get; }
+
+        protected Token(Types type)
+        {
             Type = type;
         }
 
@@ -81,68 +90,84 @@ namespace Y86.Assemable {
     }
 
     // ID 类型的Token，携带id信息
-    public class IDToken: Token {
-        public string ID {get;}
-        public IDToken(string id): base(Types.ID) => ID = id;
+    public class IDToken : Token
+    {
+        public string ID { get; }
+        public IDToken(string id) : base(Types.ID) => ID = id;
     }
 
     // Int32类型的Token，携带数值
-    public class Int32Token: Token {
-        public Int32 Value {get;}
-        public Int32Token(Int32 val): base(Types.Int32) => Value = val;
+    public class Int32Token : Token
+    {
+        public Int32 Value { get; }
+        public Int32Token(Int32 val) : base(Types.Int32) => Value = val;
     }
 
-    public interface ITokenStream {
+    public interface ITokenStream
+    {
         public Token Lookahead(int n = 1);
         public void Consume(int n = 1);
     }
 
-    public class AbstractInstruction {
-        public UInt32 Address {get;}
+    public class AbstractInstruction
+    {
+        public UInt32 Address { get; }
         public AbstractInstruction(UInt32 addr) => Address = addr;
     }
 
-    public class AIL {
+    public class AIL
+    {
         public List<AbstractInstruction> Instructions = new();
         public Dictionary<string, UInt32> Symbols = new();
     }
 
-    public class Parser {
-        public static AIL Parse(ITokenStream s) {
+    public class Parser
+    {
+        public static AIL Parse(ITokenStream s)
+        {
             AIL ail = new();    // 存放解析结果
             UInt32 address = 0; // 记录指令地址
 
             // 当token流未结束时连续的解析token
             // 允许出现在语句头的有whitespace/label/伪指令/opcode，这些token可以作为先导
             // 其它类型的token不能作为先导，出现在语句头部时表示输入为非法源代码
-            while (s.Lookahead() != Token.EOS) {
-
-                // 向前看一个token，如果是EOS则结束解析，否则使用这个token制导
+            //
+            // 每轮解析，向前看一个token指导
+            while (s.Lookahead() != Token.EOS)
+            {
                 Token tk = s.Lookahead();
-                
-                if (tk == Token.Dot) { // 伪指令形如.POS,以一个"."作为先导
-                    // 尝试去匹配一个伪指令
+
+                // 伪指令形如".POS 0x100",以一个"."开始，向前看到Dot符号则尝试匹配一个伪指令
+                // 伪指令指导汇编器的行为，成功识别到一个伪指令后需要立刻执行伪指令的操作
+                if (tk == Token.Dot)
+                {
+
                 }
-                
+
                 // label/opcode都以一个id作为先导
-                if (tk.Type == Token.Types.ID) {
-                    
+                if (tk.Type == Token.Types.ID)
+                {
                     // 向前再看一个符号，如果是":"则识别为label，否则尝试匹配一个opcode
-                    if (s.Lookahead(2) == Token.Colon) {
+                    // 注意，不允许label的重复定义
+                    if (s.Lookahead(2) == Token.Colon)
+                    {
                         string label = MatchLabel(s);
-                        if (ail.Symbols.ContainsKey(label)) {
-                            // label redefined,不允许同一个label被定义两次，报错
+                        if (ail.Symbols.ContainsKey(label))
+                        {
                             throw new Errors.LabelRedefinedException(label);
                         }
                         ail.Symbols.Add(label, address);
-                    } else {
-                        
+                    }
+                    else
+                    {
+
                     }
                     continue;
                 }
 
                 // 对于所有可以用于识别的先导token前的空白符号，都可以跳过
-                if (tk == Token.Whitespace) {
+                if (tk == Token.Whitespace)
+                {
                     s.Consume();
                     continue;
                 }
@@ -153,13 +178,15 @@ namespace Y86.Assemable {
             return ail;
         }
 
-        static string MatchLabel(ITokenStream s) {
+        static string MatchLabel(ITokenStream s)
+        {
             IDToken? tk = s.Lookahead() as IDToken;
-            if (tk != null) {
+            if (tk != null)
+            {
                 s.Consume(2); // 吃掉一个id和一个冒号
                 return tk.ID;
             }
             throw new Errors.MismatchException(Token.Types.ID, s.Lookahead());
-        } 
+        }
     }
 }
